6a76365f3905e927ac26a78e1d68378f3b95429f
luca <luca2001dambro@gmail.com>
2022-02-23 12:38:59 +0100

    __INIT__ Push

---
+++ calcolator.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+
+class Calculator:
+    def __init__(self):
+        seld.window = tk.Tk()
+        self.window.geometry("375x667")
---
+++ tkinder.py
@@ -0,0 +1,7 @@
+#release 3.7
+#TOOLKIT PYTHON TKINDER
+
+import tkinter as tk
+
+#creare una finestra(ROOT)
+window = tk.Tk()
