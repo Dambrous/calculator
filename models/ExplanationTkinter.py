68b55cb7e895508d4b5bdbe98ee9b8dc35801e4d
luca <luca2001dambro@gmail.com>
2022-02-23 13:47:57 +0100

    Pushing Changes and Fix #import tKinter ad tk

--- models/ExplanationTkinter.py
+++ models/ExplanationTkinter.py
@@ -17,6 +17,18 @@ window.geometry("600x600")
 
 #Title of window
 window.title("Ciao A Tutti")
+#Customize the window
+window.resizable(False,False)
+window.configure(background="orange")
+
+#Widget-Button(To call up functions)
+first_button = tk.Button(text="Questa è un calcolatrice")
+
+#To Place It(Geometry)(System of rows and columns)
+first_button.grid(row=0,column=0)
+
+if __name__ == "__main__":
+    window.mainloop()
 
 #How to display the window
-window.mainloop()
+# window.mainloop()
